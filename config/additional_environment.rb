4f0f2942f0edd0e7052d59847d28ba14e5d695be
Alex Chien <alex.chien@koocaa.com>
2011-09-29 17:33:09 +0800

    add additonal_environment and session_store to git

---
+++ config/additional_environment.rb
@@ -0,0 +1,17 @@
+# Copy this file to additional_environment.rb and add any statements
+# that need to be passed to the Rails::Initializer.  `config` is
+# available in this context.
+#
+# Example:
+#
+#   config.log_level = :debug
+#   config.gem "example_plugin", :lib => false
+#   config.gem "timesheet_plugin", :lib => false, :version => '0.5.0'
+#   config.gem "aws-s3", :lib => "aws/s3"
+#   ...
+#
+
+config.gem 'capistrano'
+config.gem 'capistrano-ext'
+config.gem 'mongrel_cluster'
+config.gem 'i18n', :version => '0.4.2'
