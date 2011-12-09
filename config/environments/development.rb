f232b7a23b4faf41cbca836b26c0e82810fde04c
Alex Chien <alex.chien@koocaa.com>
2011-12-09 18:15:38 +0800

    hide development based gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,6 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-config.gem 'capistrano'
-config.gem 'capistrano-ext', :lib => 'capistrano'
-config.gem 'mongrel_cluster', :lib => 'mongrel'
+# config.gem 'capistrano'
+# config.gem 'capistrano-ext', :lib => 'capistrano'
+# config.gem 'mongrel_cluster', :lib => 'mongrel'
